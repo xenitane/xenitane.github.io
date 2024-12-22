7d8f54b6b1fe112a89e828e7353e569405c1a0ec
xenitane <tushar01.tjdsk@gmail.com>
2024-12-22 23:54:26 +0530

    updated about for new job

--- src/lib/minifySlug.ts
+++ src/lib/minifySlug.ts
@@ -1,6 +1,6 @@
 import { hash } from "crypto";
 
-export default function minifuSlug(slug: string): string {
+export default function minifySlug(slug: string): string {
     if (slug.length <= 32) return slug;
     return slug.slice(0, 23) + "-" + hash("sha256", slug, "hex").slice(0, 8);
 }
